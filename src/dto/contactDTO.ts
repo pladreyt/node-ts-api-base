572a9b9ab1d7731f691c900d6205a442b0ad7b14
Pablo Ladreyt <pabloladreyt@gmail.com>
2022-02-18 09:29:10 -0300

    Capitalize Errors

--- src/dto/contactDTO.ts
+++ src/dto/contactDTO.ts
@@ -1,27 +1,27 @@
 import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class ContactDTO {
-  @MaxLength(40, { message: 'email cannot have more than $constraint1 characters' })
+  @MaxLength(40, { message: 'Email cannot have more than $constraint1 characters' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @MaxLength(20, { message: 'firstName cannot have more than $constraint1 characters' })
+  @MaxLength(20, { message: 'First name cannot have more than $constraint1 characters' })
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
-  @MaxLength(20, { message: 'lastName cannot have more than $constraint1 characters' })
+  @MaxLength(20, { message: 'Last name cannot have more than $constraint1 characters' })
   @IsString()
   @IsNotEmpty()
   lastName: string;
 
-  @MaxLength(40, { message: 'subject cannot have more than $constraint1 characters' })
+  @MaxLength(40, { message: 'Subject cannot have more than $constraint1 characters' })
   @IsString()
   @IsNotEmpty()
   subject: string;
 
-  @MaxLength(300, { message: 'body cannot have more than $constraint1 characters' })
+  @MaxLength(300, { message: 'Body cannot have more than $constraint1 characters' })
   @IsString()
   @IsNotEmpty()
   body: string;
