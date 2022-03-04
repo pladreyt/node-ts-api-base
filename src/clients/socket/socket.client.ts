674d22115e41ec3ba7fc823a2801ecd1ffa456ae
Pablo Ladreyt <pabloladreyt@gmail.com>
2022-03-04 11:48:43 -0300

    Fix type socket client

--- src/clients/socket/socket.client.ts
+++ src/clients/socket/socket.client.ts
@@ -1,12 +1,14 @@
 import { Server } from 'socket.io';
+import express from 'express';
+
 const http = require('http');
 
-export const createSocketClient = ( app: any ) => {
+export const createSocketClient = (app: express.Express) => {
   const server = http.createServer(app);
   const socketClient = new Server(server);
 
-  socketClient.on('connection', (socket) => {
-    socket.on('signin', (data) => {
+  socketClient.on('connection', socket => {
+    socket.on('signin', data => {
       const userId = data.userId;
       socket.join(`user:${userId}`);
     });
